ce5bc8069f829dd731b488db996bed6df65412f0
Luiz Fernando Libardi de Sá <luizfernando@MMB027830CPS.local>
2022-02-23 18:20:13 -0300

    contadores

---
+++ curso-pandas/contadores.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+s = pd.Series(list('dkaoskodasko'))
+print(s)
+
+# print(s.unique())
+
+print(s.value_counts())
+
+dados = pd.read_csv('./curso-pandas/files/aluguel.csv', sep = ';')
+print(dados.Tipo.unique())
+print(dados.Tipo.value_counts())
